dfee8dc6078ac3e1372c303ce5a799656df0b15f
Suyash Singh <f20240611@pilani.bits-pilani.ac.in>
2026-07-03 15:58:24 +0530

    last commit on week 6

--- scratch/check_iran.py
+++
@@ -1,9 +0,0 @@
-import json
-from pathlib import Path
-
-GAZETTEER_PATH = Path(r"C:\xampp\htdocs\aiws-weather-parser\data\form_subdivisions_gazetteer.json")
-gazetteer = json.loads(GAZETTEER_PATH.read_text(encoding="utf-8"))
-
-for item in gazetteer:
-    if "iran" in item["name"].lower():
-        print(item)
